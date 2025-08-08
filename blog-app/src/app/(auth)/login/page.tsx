e5b170b87f509558d07f7cb6ac94de860de7a436
sayedmuzammil <smuzammil87@gmail.com>
2025-08-08 14:55:40 +0700

    update

--- blog-app/src/app/(auth)/login/page.tsx
+++ blog-app/src/app/(auth)/login/page.tsx
@@ -1,8 +1,10 @@
-// import { Label } from '@radix-ui/react-label';
+'use client';
+
 import React from 'react';
-import LabelDemo from '@/components/ui/label';
 import { Form } from 'radix-ui';
 import { Button } from '@/components/ui/button';
+import { unstable_PasswordToggleField as PasswordToggleField } from 'radix-ui';
+import { EyeClosedIcon, EyeOpenIcon } from '@radix-ui/react-icons';
 
 const inputFields = [
   {
@@ -23,39 +25,68 @@ const inputFields = [
 ];
 
 const LoginPage = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+
   return (
-    <div className="w-full min-h-screen bg-var[--color-background] dark:bg-var[--colordarkBackground]">
-      <div className="w-[345px] h-[400px] flex items-center justify-center p-6 gap-5 border-2 border-dashed border-gray-300 dark:border-gray-600">
-        <Form.Root className="FormRoot">
+    <div className="flex items-center justify-center w-full min-h-screen bg-var[--color-background] dark:bg-var[--colordarkBackground]">
+      <div className="w-[345px] flex flex-col items-start justify-between p-6 border-2 border-gray-300 shadow-lg dark:border-gray-600">
+        <div className="text-md font-bold ">Sign In</div>
+        <Form.Root className="FormRoot w-full">
           {inputFields.map((field) => (
             <Form.Field key={field.id} name={field.id}>
-              <Form.Label>{field.title}</Form.Label>
-              <Form.Message match="valueMissing" className="text-red-500">
-                {field.valueMissing}
-              </Form.Message>
-              <Form.Control asChild>
-                <input
-                  type={field.type}
-                  placeholder={field.placeholder}
-                  required
-                />
-              </Form.Control>
+              <div className="flex flex-col gap-1 mt-5">
+                <div className="flex flex-row justify-between gap-2">
+                  <Form.Label className="text-sm font-semibold">
+                    {field.title}
+                  </Form.Label>
+                  <Form.Message
+                    match="valueMissing"
+                    className="text-red-500 text-xs"
+                  >
+                    {field.valueMissing}
+                  </Form.Message>
 
-              {field.typeMismatch && (
-                // Only show typeMismatch message if it exists
-                <Form.Message match="typeMismatch">
-                  {field.typeMismatch}
-                </Form.Message>
-              )}
+                  {field.typeMismatch && (
+                    // Only show typeMismatch message if it exists
+                    <Form.Message
+                      match="typeMismatch"
+                      className="text-red-500 text-xs"
+                    >
+                      {field.typeMismatch}
+                    </Form.Message>
+                  )}
+                </div>
+                <Form.Control asChild>
+                  <input
+                    type={field.type}
+                    placeholder={field.placeholder}
+                    required
+                    className="w-full px-3 py-2 border border-neutral-300 rounded-2xl "
+                  />
+                </Form.Control>
+              </div>
             </Form.Field>
           ))}
 
           <Form.Submit asChild>
-            <Button variant="default" type="submit">
+            <Button variant="default" type="submit" className="mt-6">
               Login
             </Button>
           </Form.Submit>
         </Form.Root>
+        <div className="flex items-center justify-center w-full mt-4">
+          <div className="text-sm">
+            Dont have an account?{' '}
+            <span>
+              <a
+                href="/register"
+                className="text-[var(--color-secondary)] underline text-sm font-semibold"
+              >
+                Register
+              </a>
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
